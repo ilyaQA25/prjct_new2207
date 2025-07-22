ca2a2bf86748d8b27bb017a486705f864d8ebbec
ilya2.0 <iprez4031@gmail.com>
2025-07-22 16:24:18 +0300

    new-prjct-july25

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String countrise [] = {"KNDR", "USA", "RF"};
+        for (int i = countrise.length-1; i >= 0; i--) {
+            System.out.println(countrise[i]);
+        }
+        }
+    }
